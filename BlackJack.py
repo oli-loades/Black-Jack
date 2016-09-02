230e6456ff2d1ed4fb9a276ed4c9844e1d86601d
oli-loades <oli.loades@gmail.com>
2016-09-02 17:45:13 +0100

    Update BlackJack.py
    added future menu options and restructured the program

--- BlackJack.py
+++ BlackJack.py
@@ -27,27 +27,54 @@ def deal(deckPos, deck):
 def ace():
     yn = ""
     while yn != "y" or yn != "n":
-        #yn = input("you got an ace, do you want to it to count as 11 [y/n]?")
+        yn = input("you got an ace, do you want to it to count as 11 [y/n]?")
         if yn == "y":
             return 11
         elif yn == "n":
             return 1
+        
+def displayMenu():
+    print "Option 1 - Play BlackJack"
+    print "Option 2 - View Tokens"
+    print "Option 3 - Save"
+    print "Option 4 - Load"
+    print "Option 0 - Ecit"
 
-deckPos = 0 #removes the need to remove cards from the deck array
-pTotal = 0
-dTotal = 0;
-deck = []
-deck = createDeck()
+def game():
+    deckPos = 0 #removes the need to remove cards from the deck array
+    pTotal = 0
+    dTotal = 0;
+    deck = []
+    hitOrStick = ""
+    deck = createDeck()
 
-deck = shuffleDeck(deck)
-for i in range(0,2):
-    score = deal(deckPos, deck)
-    pTotal = pTotal + score
-    deckPos+=1
+    deck = shuffleDeck(deck)
+    for i in range(0,2):
+        score = deal(deckPos, deck)
+        pTotal = pTotal + score
+        deckPos+=1
 
-    score = deal(deckPos, deck)
-    dTotal = dTotal + score
-    deckPos+=1
+        score = deal(deckPos, deck)
+        dTotal = dTotal + score
+        deckPos+=1
 
-print "players score total:" + str(pTotal)
-print "dealers score total:" + str(dTotal)
+    print "players score total:" + str(pTotal)
+    print "dealers score total:" + str(dTotal)
+
+    #did they get blackjack?
+    #if not
+    #ask user if teh ywant to stick or hit
+    #repeat until user goes above 21 or chooses to stick
+
+def main():
+    userOption = -1
+    while userOption != 0:
+        userOption = intput("enter an option from the menu")
+        displayMenu()
+        if userOption == 1:
+            game();
+    
+    
+if __name__ == "__main__":
+	while True:
+            main()
